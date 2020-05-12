e5b452361de385e1a471946b37b4e9f1de5b5e91
Mauricio R Duarte <mauriciotem@gmail.com>
2020-05-11 23:52:13 -0300

    fix: removed console log

--- src/services/01api/api.ts
+++ src/services/01api/api.ts
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 import { API_01 } from '../../shared/constants/services';
 
-console.log(API_01);
-
 const api = axios.create({
   baseURL: API_01.API_URL,
   headers: {
